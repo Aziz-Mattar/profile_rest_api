c7fd5f1dee109f1e2a1636597a732f557ac94b45
Aziz-Mattar <aziz.balaha@hotmail.com>
2022-08-16 13:05:37 +0300

    configuered for deployment on aws

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'wyx-z-e1)0)d&mvg6g6bu6dk!=h4s==yb$jo2xx&hm_gnty)k4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = []
 
@@ -121,4 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
